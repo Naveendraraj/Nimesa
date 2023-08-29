cdfecc30a105a69d63d25dbed1fb0c75738bf931
Naveendraraj <122799618+Naveendraraj@users.noreply.github.com>
2023-08-29 22:38:33 +0530

    Create Assignment1.tf

---
+++ Assignment1.tf
@@ -0,0 +1,55 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow ssh inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-09e67e426f25ce0d7"
+  instance_type = "t2.micro"
+
+  root_block_device {
+    volume_size           = 5
+    volume_type           = "gp2"
+    delete_on_termination = true
+  }
+
+  tags = {
+    purpose = "Assignment"
+  }
+
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  subnet_id              = aws_subnet.public.id
+}
